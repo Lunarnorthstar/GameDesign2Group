295e1a67b50a001e5098634d6fbfc4593f17f521
MN-Cool <c20334611@mytudublin.ie>
2021-09-27 15:04:01 +0100

    Would this be any better?

--- GameDesignProtoype/Assets/Scripts/UI_TextInputWindow.cs
+++ GameDesignProtoype/Assets/Scripts/UI_TextInputWindow.cs
@@ -1,17 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UI_TextInputWindow : MonoBehaviour
 {
-    private void Awake()
+    public string minion1Name;
+    public string minion2Name;
+    public string minion3Name;
+    public TMP_InputField name1;
+    public TMP_InputField name2;
+    public TMP_InputField name3;
+    public GameObject minion1;
+    public GameObject minion2;
+    public GameObject minion3;
+
+    void Start()
     {
-        Hide();
+        name1.GetComponent<Text>();
+        name2.GetComponent<Text>();
+        name3.GetComponent<Text>();
     }
 
-    public void Show()
+    public void StoreName()
     {
-        gameObject.SetActive(true);
+        name1.text = minion1Name;
+        name2.text = minion2Name;
+        name3.text = minion3Name;
+        minion1Name = minion1.name;
+        minion2Name = minion2.name;
+        minion3Name = minion3.name;
     }
 
     public void Hide()
