5d3eb1075bebffa19cc12a616246b84b5c378a51
D19127919 <77803253+D19127919@users.noreply.github.com>
2021-09-28 19:24:36 +0100

    Final tweaks to demo level
    The only non-working feature is the UI element of naming the minions (as far as I'm aware at least)

--- GameDesignProtoype/Assets/Scripts/doorScript.cs
+++ GameDesignProtoype/Assets/Scripts/doorScript.cs
@@ -13,7 +13,7 @@ public class doorScript : MonoBehaviour
     public GameObject door;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player")) //If the player bumps into you...
+        if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Minion1")) //If the player bumps into you...
         {
             door.SetActive(false); //Make door go away
         }
