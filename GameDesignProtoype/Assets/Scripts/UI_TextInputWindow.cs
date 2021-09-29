0d36b1146b1dc209e1a1421a7ca3658677d55fd2
MN-Cool <c20334611@mytudublin.ie>
2021-09-29 19:04:49 +0100

    Adding UI Text

--- GameDesignProtoype/Assets/Scripts/UI_TextInputWindow.cs
+++ GameDesignProtoype/Assets/Scripts/UI_TextInputWindow.cs
@@ -6,31 +6,19 @@ using TMPro;
 
 public class UI_TextInputWindow : MonoBehaviour
 {
-    public string minion1Name;
-    public string minion2Name;
-    public string minion3Name;
-    public TMP_InputField name1;
-    public TMP_InputField name2;
-    public TMP_InputField name3;
-    public TMP_Text minion1Tag;
-    public TMP_Text minion2Tag;
-    public TMP_Text minion3Tag;
+    public string minionName;
+    public TMP_InputField newName;
+    public TMP_Text minionTag;
 
     void Start()
     {
-        name1.GetComponent<Text>();
-        name2.GetComponent<Text>();
-        name3.GetComponent<Text>();
+        newName.GetComponent<Text>();
     }
 
     public void StoreName()
     {
-        minion1Name = minion1Tag.text;
-        minion2Name = minion2Tag.text;
-        minion3Name = minion3Tag.text;
-        minion1Tag.text = name1.text;
-        minion2Tag.text = name2.text;
-        minion3Tag.text = name3.text;
+        minionName = minionTag.text;
+        minionTag.text = newName.text;
     }
 
     public void Hide()
