b14eb586cee0880e98f20327b4bb6353d12ad62c
Glopex <62385400+Glopex@users.noreply.github.com>
2021-10-01 14:41:20 +0100

    final prototype

--- GameDesignProtoype/Assets/Scripts/UI_TextInputWindow.cs
+++ GameDesignProtoype/Assets/Scripts/UI_TextInputWindow.cs
@@ -19,6 +19,7 @@ public class UI_TextInputWindow : MonoBehaviour
     {
         minionName = minionTag.text;
         minionTag.text = newName.text;
+        GameObject.Find("Player").SendMessage("ActivateMovement");
     }
 
     public void Hide()
