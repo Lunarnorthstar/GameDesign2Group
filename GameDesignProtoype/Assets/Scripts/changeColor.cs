9078f63e6c3a6957fc5b981c23af1031b352bf76
MN-Cool <c20334611@mytudublin.ie>
2021-10-05 20:30:08 +0100

    Death Colors for Minions, and Camera & Text changes

--- GameDesignProtoype/Assets/Scripts/changeColor.cs
+++ GameDesignProtoype/Assets/Scripts/changeColor.cs
@@ -4,39 +4,28 @@ using UnityEngine;
 
 public class changeColor : MonoBehaviour
 {
-
     [SerializeField] private Color MM1 = Color.white;
     [SerializeField] private Color MM1A = Color.white;
-    
+    [SerializeField] private Color MM1I = Color.white;
+
     public SpriteRenderer myObject;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
         myObject = GetComponent<SpriteRenderer>();
     }
     
     public void changeColorOn()
     {
-       
-            
-            
-            myObject.material.color = MM1A;
-        
-        
+        myObject.material.color = MM1A;
     }
     public void changeColorOff()
     {
-        
-            Debug.Log("imma go stupid");
-           
-            myObject.material.color = MM1;
-        
-
+        Debug.Log("imma go stupid");  
+        myObject.material.color = MM1;
     }
-
-
-
-
+    public void changeColorDead()
+    {
+        myObject.material.color = MM1I;
+    }
 }
--- GameDesignProtoype/Assets/Scripts/killZone.cs
+++ GameDesignProtoype/Assets/Scripts/killZone.cs
@@ -5,6 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class killZone : MonoBehaviour
 {
+    public GameObject minionDead1;
+    public GameObject minionDead2;
+    public GameObject minionDead3;
+
+    void Start()
+    {
+        minionDead1 = GameObject.Find("Minion (1)").gameObject.transform.GetChild(0).gameObject;
+        minionDead2 = GameObject.Find("Minion (2)").gameObject.transform.GetChild(0).gameObject;
+        minionDead3 = GameObject.Find("Minion (3)").gameObject.transform.GetChild(0).gameObject;
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
@@ -15,16 +26,19 @@ public class killZone : MonoBehaviour
         if (other.gameObject.CompareTag("Minion1"))
         {
             GameObject.FindWithTag("Minion1").SendMessage("DeactivateMinion");
+            minionDead1.SendMessage("changeColorDead");
         }
         
         if (other.gameObject.CompareTag("Minion2"))
         {
             GameObject.FindWithTag("Minion2").SendMessage("DeactivateMinion");
+            minionDead2.SendMessage("changeColorDead");
         }
         
         if (other.gameObject.CompareTag("Minion3"))
         {
             GameObject.FindWithTag("Minion3").SendMessage("DeactivateMinion");
+            minionDead3.SendMessage("changeColorDead");
         } //These three perma-kill each minion when they touch it. Needed 1 for each minion because I don't know how to format it more efficiently
     }
 }
