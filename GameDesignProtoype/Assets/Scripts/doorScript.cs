3bac8fd532f5c657a0f9d3ea631463f0a6fe373d
MN-Cool <c20334611@mytudublin.ie>
2021-10-09 18:40:08 +0100

    Making a button sprite

--- GameDesignProtoype/Assets/Scripts/doorScript.cs
+++ GameDesignProtoype/Assets/Scripts/doorScript.cs
@@ -11,10 +11,20 @@ public class doorScript : MonoBehaviour
 {
     public Boolean IsToggleDoor = false;
     public GameObject door;
+    public SpriteRenderer myButton;
+    public Sprite buttonOn;
+    public Sprite buttonOff;
+
+    void Start()
+    {
+        myButton = GetComponent<SpriteRenderer>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Minion1") || other.gameObject.CompareTag("Minion2") || other.gameObject.CompareTag("Minion3")) //If the player bumps into you...
         {
+            myButton.sprite = buttonOn;
             door.SetActive(false); //Make door go away
         } //Apparently the separate tags for each minion were necessary. I don't know why as of writing this comment, but I was told it was necessary to have a unique tag on each minion.
     }
@@ -24,6 +34,7 @@ public class doorScript : MonoBehaviour
         if (IsToggleDoor == true) //If a box in the editor is checked...
         {
             door.SetActive(true); //Come back when they stop bumping you
+            myButton.sprite = buttonOff;
         }
     }
 }
